e63267463484496f6fb4c4ff983d052f52959304
Zachve4037 <matej.zachveja@gmail.com>
2024-12-29 15:58:18 +0100

    Trying

--- ipc_utils.c
+++ ipc_utils.c
@@ -1,3 +1,59 @@
-//
-// Created by matej on 29. 12. 2024.
-//
+#include "ipc_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int create_server_socket() {
+    int server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    unlink(SOCKET_PATH);
+    if (bind(server_socket, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, MAX_CLIENTS) == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    return server_socket;
+}
+
+int accept_client(int server_socket) {
+    return accept(server_socket, NULL, NULL);
+}
+
+int connect_to_server(const char *path) {
+    int client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+
+    if (connect(client_socket, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    return client_socket;
+}
+
+void close_socket(int socket_fd) {
+    close(socket_fd);
+}
